f8da2bd21b06e058c29d64fde88fcd5341bacb9c
Super12138 <70494801+Super12138@users.noreply.github.com>
2022-09-11 13:56:59 +0800

    在浏览器中打开链接

---
+++ js/renderer.js
@@ -0,0 +1,8 @@
+const { shell } = require("electron");
+
+let uplink = document.querySelector("#uplink");
+uplink.onclick = (e) => {
+    e.preventDefault();
+    let href = uplink.getAttribute("href");
+    shell.openExternal(href);
+}
