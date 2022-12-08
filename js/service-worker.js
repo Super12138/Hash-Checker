13aa5e764d069e7c0bedc4ed4a026835062402e1
Super12138 <70494801+Super12138@users.noreply.github.com>
2022-12-08 14:01:01 +0800

    更新网站

--- js/service-worker.js
+++ js/service-worker.js
@@ -1,66 +1,68 @@
 
-    // Based off of https://github.com/pwa-builder/PWABuilder/blob/main/docs/sw.js
+// Based off of https://github.com/pwa-builder/PWABuilder/blob/main/docs/sw.js
 
-    /*
-      Welcome to our basic Service Worker! This Service Worker offers a basic offline experience
-      while also being easily customizeable. You can add in your own code to implement the capabilities
-      listed below, or change anything else you would like.
+/*
+  Welcome to our basic Service Worker! This Service Worker offers a basic offline experience
+  while also being easily customizeable. You can add in your own code to implement the capabilities
+  listed below, or change anything else you would like.
 
 
-      Need an introduction to Service Workers? Check our docs here: https://docs.pwabuilder.com/#/home/sw-intro
-      Want to learn more about how our Service Worker generation works? Check our docs here: https://docs.pwabuilder.com/#/studio/existing-app?id=add-a-service-worker
+  Need an introduction to Service Workers? Check our docs here: https://docs.pwabuilder.com/#/home/sw-intro
+  Want to learn more about how our Service Worker generation works? Check our docs here: https://docs.pwabuilder.com/#/studio/existing-app?id=add-a-service-worker
 
-      Did you know that Service Workers offer many more capabilities than just offline? 
-        - Background Sync: https://microsoft.github.io/win-student-devs/#/30DaysOfPWA/advanced-capabilities/06
-        - Periodic Background Sync: https://web.dev/periodic-background-sync/
-        - Push Notifications: https://microsoft.github.io/win-student-devs/#/30DaysOfPWA/advanced-capabilities/07?id=push-notifications-on-the-web
-        - Badges: https://microsoft.github.io/win-student-devs/#/30DaysOfPWA/advanced-capabilities/07?id=application-badges
-    */
+  Did you know that Service Workers offer many more capabilities than just offline? 
+    - Background Sync: https://microsoft.github.io/win-student-devs/#/30DaysOfPWA/advanced-capabilities/06
+    - Periodic Background Sync: https://web.dev/periodic-background-sync/
+    - Push Notifications: https://microsoft.github.io/win-student-devs/#/30DaysOfPWA/advanced-capabilities/07?id=push-notifications-on-the-web
+    - Badges: https://microsoft.github.io/win-student-devs/#/30DaysOfPWA/advanced-capabilities/07?id=application-badges
+*/
 
-    const HOSTNAME_WHITELIST = [
-        self.location.hostname,
-        'cdn.jsdelivr.net'
-    ]
+const HOSTNAME_WHITELIST = [
+    self.location.hostname,
+    'fonts.gstatic.com',
+    'fonts.googleapis.com',
+    'cdn.jsdelivr.net'
+]
 
-    // The Util Function to hack URLs of intercepted requests
-    const getFixedUrl = (req) => {
-        var now = Date.now()
-        var url = new URL(req.url)
+// The Util Function to hack URLs of intercepted requests
+const getFixedUrl = (req) => {
+    var now = Date.now()
+    var url = new URL(req.url)
 
-        // 1. fixed http URL
-        // Just keep syncing with location.protocol
-        // fetch(httpURL) belongs to active mixed content.
-        // And fetch(httpRequest) is not supported yet.
-        url.protocol = self.location.protocol
+    // 1. fixed http URL
+    // Just keep syncing with location.protocol
+    // fetch(httpURL) belongs to active mixed content.
+    // And fetch(httpRequest) is not supported yet.
+    url.protocol = self.location.protocol
 
-        // 2. add query for caching-busting.
-        // Github Pages served with Cache-Control: max-age=600
-        // max-age on mutable content is error-prone, with SW life of bugs can even extend.
-        // Until cache mode of Fetch API landed, we have to workaround cache-busting with query string.
-        // Cache-Control-Bug: https://bugs.chromium.org/p/chromium/issues/detail?id=453190
-        if (url.hostname === self.location.hostname) {
-            url.search += (url.search ? '&' : '?') + 'cache-bust=' + now
-        }
-        return url.href
+    // 2. add query for caching-busting.
+    // Github Pages served with Cache-Control: max-age=600
+    // max-age on mutable content is error-prone, with SW life of bugs can even extend.
+    // Until cache mode of Fetch API landed, we have to workaround cache-busting with query string.
+    // Cache-Control-Bug: https://bugs.chromium.org/p/chromium/issues/detail?id=453190
+    if (url.hostname === self.location.hostname) {
+        url.search += (url.search ? '&' : '?') + 'cache-bust=' + now
     }
+    return url.href
+}
 
-    /**
-     *  @Lifecycle Activate
-     *  New one activated when old isnt being used.
-     *
-     *  waitUntil(): activating ====> activated
-     */
-    self.addEventListener('activate', event => {
-      event.waitUntil(self.clients.claim())
-    })
+/**
+ *  @Lifecycle Activate
+ *  New one activated when old isnt being used.
+ *
+ *  waitUntil(): activating ====> activated
+ */
+self.addEventListener('activate', event => {
+    event.waitUntil(self.clients.claim())
+})
 
-    /**
-     *  @Functional Fetch
-     *  All network requests are being intercepted here.
-     *
-     *  void respondWith(Promise<Response> r)
-     */
-    self.addEventListener('fetch', event => {
+/**
+ *  @Functional Fetch
+ *  All network requests are being intercepted here.
+ *
+ *  void respondWith(Promise<Response> r)
+ */
+self.addEventListener('fetch', event => {
     // Skip some of cross-origin requests, like those for Google Analytics.
     if (HOSTNAME_WHITELIST.indexOf(new URL(event.request.url).hostname) > -1) {
         // Stale-while-revalidate
@@ -76,16 +78,16 @@
         // If there’s nothing in cache, wait for the fetch.
         // If neither yields a response, return offline pages.
         event.respondWith(
-        Promise.race([fetched.catch(_ => cached), cached])
-            .then(resp => resp || fetched)
-            .catch(_ => { /* eat any errors */ })
+            Promise.race([fetched.catch(_ => cached), cached])
+                .then(resp => resp || fetched)
+                .catch(_ => { /* eat any errors */ })
         )
 
         // Update the cache with the version we fetched (only for ok status)
         event.waitUntil(
-        Promise.all([fetchedCopy, caches.open("pwa-cache")])
-            .then(([response, cache]) => response.ok && cache.put(event.request, response))
-            .catch(_ => { /* eat any errors */ })
+            Promise.all([fetchedCopy, caches.open("pwa-cache")])
+                .then(([response, cache]) => response.ok && cache.put(event.request, response))
+                .catch(_ => { /* eat any errors */ })
         )
     }
-    })
+})
