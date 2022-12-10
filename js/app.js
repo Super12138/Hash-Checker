22f39a705cd6f9910237f4d075c9d8effdcd1297
Super12138 <70494801+Super12138@users.noreply.github.com>
2022-12-10 16:50:43 +0800

    简化代码

--- js/app.js
+++ js/app.js
@@ -1,8 +1,12 @@
-const { 
+const {
     clipboard,
     shell
 } = require('electron')
-  
+
+//let ctypto = require('crypto');
+//let xxx = crypto.createHash('sha256').update(file).digest('hex');
+//console.log(xxx);
+
 const links = document.querySelectorAll('a[href]');
 links.forEach(link => {
     link.addEventListener('click', e => {
@@ -112,32 +116,23 @@ function calch() {
                         reader.onload = function () {
                             var calcmethod = document.querySelector('#method').value;
                             tips.innerHTML = "获取到计算" + calcmethod + "值";
-                            if (calcmethod == "md5") {
-                                tips.innerHTML = "正在计算...";
-                                var wordArray = CryptoJS.lib.WordArray.create(reader.result);
+                            tips.innerHTML = "正在计算...";
+                            var wordArray = CryptoJS.lib.WordArray.create(reader.result);
+                            //化简代码
+                            if (calcmethod == 'md5') {
                                 var calchash = CryptoJS.MD5(wordArray).toString(CryptoJS.enc.Hex);
-                                console.log(calchash);
-                                var ch = calchash;
-                                var f = document.getElementById('a').value;
-                                var a = f.toLowerCase();
-                                var b = ch.toLowerCase();
-                                var calcmethod = document.querySelector('#method').value;
-                                clipboard.writeText(calchash);
-                                tips.innerHTML = "计算完成，" + calcmethod + "值已写入您的剪贴板！<br>" + calcmethod + "值：" + "<code>" +  calchash + "</code>";
                             }
                             else {
-                                tips.innerHTML = "正在计算...";
-                                var wordArray = CryptoJS.lib.WordArray.create(reader.result);
                                 var calchash = CryptoJS.SHA256(wordArray).toString(CryptoJS.enc.Hex);
-                                console.log(calchash);
-                                var ch = calchash;
-                                var f = document.getElementById('a').value;
-                                var a = f.toLowerCase();
-                                var b = ch.toLowerCase();
-                                var calcmethod = document.querySelector('#method').value;
-                                clipboard.writeText(calchash);
-                                tips.innerHTML = "计算完成，" + calcmethod + "值已写入您的剪贴板！<br>" + calcmethod+ "值：" + "<code>" +  calchash + "</code>";
-                            };
+                            }
+                            console.log(calchash);
+                            var ch = calchash;
+                            var f = document.getElementById('a').value;
+                            var a = f.toLowerCase();
+                            var b = ch.toLowerCase();
+                            var calcmethod = document.querySelector('#method').value;
+                            clipboard.writeText(calchash);
+                            tips.innerHTML = "计算完成，" + calcmethod + "值已写入您的剪贴板！<br>" + calcmethod + "值：" + "<code>" + calchash + "</code>";
                         }
                     }
                 }
@@ -178,76 +173,45 @@ function calch() {
                             reader.onload = function () {
                                 var calcmethod = document.querySelector('#method').value;
                                 tips.innerHTML = "获取到计算" + calcmethod + "值";
-                                if (calcmethod == "md5") {
-                                    tips.innerHTML = "正在计算...";
-                                    var wordArray = CryptoJS.lib.WordArray.create(reader.result);
+                                tips.innerHTML = "正在计算...";
+                                var wordArray = CryptoJS.lib.WordArray.create(reader.result);
+                                //化简代码
+                                if (calcmethod == 'md5') {
                                     var calchash = CryptoJS.MD5(wordArray).toString(CryptoJS.enc.Hex);
-                                    console.log(calchash);
-                                    var ch = calchash;
-                                    var f = document.getElementById('a').value;
-                                    var a = f.toLowerCase();
-                                    var b = ch.toLowerCase();
-                                    var calcmethod = document.querySelector('#method').value;
-                                    if (b === a) {
-                                        tips.innerHTML = "校验完成<br>" + calcmethod + "值：" + "<code>" +  calchash + "</code>";
-                                        mdui.dialog({
-                                            title: '校验成功',
-                                            content: '请放心使用',
-                                            buttons: [
-                                                {
-                                                    text: '完成'
-                                                }
-                                            ]
-                                        });
-                                    }
-                                    else {
-                                        tips.innerHTML = "校验完成<br>" + calcmethod + "值：" + "<code>" +  calchash + "</code>";
-                                        mdui.dialog({
-                                            title: '校验失败',
-                                            content: '请检查获取的校验值是否有误（如多余的空格），或是校验值和校验方法不一致（如校验值是MD5，但校验方法是SHA256)；如果没有上述任一情况请自行解决',
-                                            buttons: [
-                                                {
-                                                    text: '确认'
-                                                }
-                                            ]
-                                        });
-                                    }
                                 }
                                 else {
-                                    tips.innerHTML = "正在计算...";
-                                    var wordArray = CryptoJS.lib.WordArray.create(reader.result);
                                     var calchash = CryptoJS.SHA256(wordArray).toString(CryptoJS.enc.Hex);
-                                    console.log(calchash);
-                                    var ch = calchash;
-                                    var f = document.getElementById('a').value;
-                                    var a = f.toLowerCase();
-                                    var b = ch.toLowerCase();
-                                    var calcmethod = document.querySelector('#method').value;
-                                    if (b === a) {
-                                        tips.innerHTML = "校验完成<br>" + calcmethod + "值：" + "<code>" +  calchash + "</code>";
-                                        mdui.dialog({
-                                            title: '校验成功',
-                                            content: '请放心使用',
-                                            buttons: [
-                                                {
-                                                    text: '完成'
-                                                }
-                                            ]
-                                        });
-                                    }
-                                    else {
-                                        tips.innerHTML = "校验完成<br>" + calcmethod + "值：" + "<code>" +  calchash + "</code>";
-                                        mdui.dialog({
-                                            title: '校验失败',
-                                            content: '请检查获取的校验值是否有误（如多余的空格），或是校验值和校验方法不一致（如校验值是MD5，但校验方法是SHA256)；如果没有上述任一情况请自行解决',
-                                            buttons: [
-                                                {
-                                                    text: '确认'
-                                                }
-                                            ]
-                                        });
-                                    }
                                 }
+                                console.log(calchash);
+                                var f = document.getElementById('a').value;
+                                var a = f.toLowerCase();
+                                var b = calchash.toLowerCase();
+                                var calcmethod = document.querySelector('#method').value;
+                                if (b === a) {
+                                    tips.innerHTML = "校验完成<br>" + calcmethod + "值：" + "<code>" + calchash + "</code>";
+                                    mdui.dialog({
+                                        title: '校验成功',
+                                        content: '请放心使用',
+                                        buttons: [
+                                            {
+                                                text: '完成'
+                                            }
+                                        ]
+                                    });
+                                }
+                                else {
+                                    tips.innerHTML = "校验完成<br>" + calcmethod + "值：" + "<code>" + calchash + "</code>";
+                                    mdui.dialog({
+                                        title: '校验失败',
+                                        content: '请检查获取的校验值是否有误（如多余的空格），或是校验值和校验方法不一致（如校验值是MD5，但校验方法是SHA256)；如果没有上述任一情况请自行解决',
+                                        buttons: [
+                                            {
+                                                text: '确认'
+                                            }
+                                        ]
+                                    });
+                                }
+
                             };
                         }
                     }
