0a24b9a065ce125619dedba3083c78bc29d22cea
Super12138 <70494801+Super12138@users.noreply.github.com>
2022-12-26 08:11:51 +0800

    国际化适配

---
+++ js/link.js
@@ -0,0 +1,18 @@
+function win64() {
+    window.location.href = 'https://github.com/Super12138/Hash-Checker/releases/download/1.0.3/Hash.Checker.Setup.1.0.3-x64.exe';
+}
+function win32() {
+    window.location.href = 'https://github.com/Super12138/Hash-Checker/releases/download/1.0.3/Hash.Checker.Setup.1.0.3-x32.exe';
+}
+function wina64() {
+    window.location.href = 'https://github.com/Super12138/Hash-Checker/releases/download/1.0.3/Hash.Checker.Setup.1.0.3-arm64.exe';
+}
+function macu() {
+    window.location.href = 'https://github.com/Super12138/Hash-Checker/releases/download/1.0.3/Hash.Checker-1.0.3-universal.dmg';
+}
+function mac() {
+    window.location.href = 'https://github.com/Super12138/Hash-Checker/releases/download/1.0.3/Hash.Checker-1.0.3.dmg';
+}
+function maca64() {
+    window.location.href = 'https://github.com/Super12138/Hash-Checker/releases/download/1.0.3/Hash.Checker-1.0.3-arm64.dmg';
+}
