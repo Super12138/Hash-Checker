aed0a2d4887f16b6f12706997cede394f5657c82
Super12138 <70494801+Super12138@users.noreply.github.com>
2023-02-06 09:58:47 +0800

    修复在只选择校验模式但是不弹出dialog的问题

--- js/app.js
+++ js/app.js
@@ -16,13 +16,12 @@ function getfilename() {
         if (file.size >= 104857600) {
             wfilename.innerHTML = file.name;
             wfilesize.innerHTML = file.size + "&nbsp; Byte";
-            wetips.innerHTML = "推荐上传小于100MB的文件";
-            console.log(file);
+            wetips.innerHTML = "文件大小较大，计算时间可能较长";
         } else {
             wfilename.innerHTML = file.name;
             wfilesize.innerHTML = file.size + "&nbsp; Byte";
-            console.log(file);
         }
+        console.log(file);
     }
 }
 
@@ -74,64 +73,24 @@ function calch() {
                 });
             }
             else {
-                if (only == "generate") {
-                    var meth = document.querySelector('#method').value;
-                    if (meth == "nullselect") {
-                        mdui.dialog({
-                            title: '错误',
-                            content: '请选择方法之后再进行操作！',
-                            buttons: [
-                                {
-                                    text: '知道了'
-                                }
-                            ]
-                        });
-                    }
-                    else {
-                        var filec = document.querySelector('#getfile');
-                        var ttitle = document.querySelector('#ttitle');
-                        var tips = document.querySelector('#tips');
-                        let reader = new FileReader();
-                        ttitle.innerHTML = "状态：";
-                        tips.innerHTML = "正在将文件缓存...";
-                        reader.readAsArrayBuffer(filec.files[0]);
-                        reader.onload = function () {
-                            var calcmethod = document.querySelector('#method').value;
-                            tips.innerHTML = "获取到计算" + calcmethod + "值";
-                            tips.innerHTML = "正在计算...";
-                            var wordArray = CryptoJS.lib.WordArray.create(reader.result);
-                            if (calcmethod == "md5") {
-                                var calchash = CryptoJS.MD5(wordArray).toString(CryptoJS.enc.Hex);
+                var meth = document.querySelector('#method').value;
+                if (meth == "nullselect") {
+                    mdui.dialog({
+                        title: '错误',
+                        content: '请选择方法之后再进行操作！',
+                        buttons: [
+                            {
+                                text: '知道了'
                             }
-                            else {
-                                var calchash = CryptoJS.SHA256(wordArray).toString(CryptoJS.enc.Hex);
-                            }
-                            console.log(calchash);
-                            var calcmethod = document.querySelector('#method').value;
-                            navigator.clipboard.writeText(calchash).catch(e => console.error(e));
-                            tips.innerHTML = "计算完成，" + calcmethod + "值已写入您的剪贴板！<br>" + calcmethod + "值：" + calchash;
-                        }
-                    }
-                }
-                else {
-                    var meth = document.querySelector('#method').value;
-                    if (meth == "nullselect") {
-                        mdui.dialog({
-                            title: '错误',
-                            content: '请选择方法之后再进行校验！',
-                            buttons: [
-                                {
-                                    text: '知道了'
-                                }
-                            ]
-                        });
-                    }
-                    else {
-                        var inpc = document.getElementById('a').value;
-                        if (inpc === "" || inpc === null || inpc === undefined) {
+                        ]
+                    });
+                } else {
+                    if (only == "generate") {
+                        var meth = document.querySelector('#method').value;
+                        if (meth == "nullselect") {
                             mdui.dialog({
                                 title: '错误',
-                                content: '请输入校验值之后再进行校验！',
+                                content: '请选择方法之后再进行操作！',
                                 buttons: [
                                     {
                                         text: '知道了'
@@ -150,46 +109,97 @@ function calch() {
                             reader.onload = function () {
                                 var calcmethod = document.querySelector('#method').value;
                                 tips.innerHTML = "获取到计算" + calcmethod + "值";
-
                                 tips.innerHTML = "正在计算...";
                                 var wordArray = CryptoJS.lib.WordArray.create(reader.result);
-                                if (calcmethod == "md5") {
+                                if (calcmethod == 'md5') {
                                     var calchash = CryptoJS.MD5(wordArray).toString(CryptoJS.enc.Hex);
                                 }
                                 else {
                                     var calchash = CryptoJS.SHA256(wordArray).toString(CryptoJS.enc.Hex);
                                 }
                                 console.log(calchash);
-                                var ch = calchash;
-                                var f = document.getElementById('a').value;
-                                var a = f.toLowerCase();
-                                var b = ch.toLowerCase();
                                 var calcmethod = document.querySelector('#method').value;
-                                if (b === a) {
-                                    tips.innerHTML = "校验完成<br>" + calcmethod + "值：" + calchash;
-                                    mdui.dialog({
-                                        title: '校验成功',
-                                        content: '请放心使用',
-                                        buttons: [
-                                            {
-                                                text: '完成'
-                                            }
-                                        ]
-                                    });
-                                }
-                                else {
-                                    tips.innerHTML = "校验完成<br>" + calcmethod + "值：" + calchash;
-                                    mdui.dialog({
-                                        title: '校验失败',
-                                        content: '请检查获取的校验值是否有误（如多余的空格），或是校验值和校验方法不一致（如校验值是MD5，但校验方法是SHA256)；如果没有上述任一情况请自行解决',
-                                        buttons: [
-                                            {
-                                                text: '确认'
-                                            }
-                                        ]
-                                    });
-                                }
-                            };
+                                navigator.clipboard.writeText(calchash).catch(e => console.error(e));
+                                tips.innerHTML = "计算完成，" + calcmethod + "值已写入您的剪贴板！<br>" + calcmethod + "值：" + calchash;
+                            }
+                        }
+                    }
+                    else {
+                        var meth = document.querySelector('#method').value;
+                        if (meth == "nullselect") {
+                            mdui.dialog({
+                                title: '错误',
+                                content: '请选择方法之后再进行校验！',
+                                buttons: [
+                                    {
+                                        text: '知道了'
+                                    }
+                                ]
+                            });
+                        }
+                        else {
+                            var inpc = document.getElementById('a').value;
+                            if (inpc === "" || inpc === null || inpc === undefined) {
+                                mdui.dialog({
+                                    title: '错误',
+                                    content: '请输入校验值之后再进行校验！',
+                                    buttons: [
+                                        {
+                                            text: '知道了'
+                                        }
+                                    ]
+                                });
+                            }
+                            else {
+                                var filec = document.querySelector('#getfile');
+                                var ttitle = document.querySelector('#ttitle');
+                                var tips = document.querySelector('#tips');
+                                let reader = new FileReader();
+                                ttitle.innerHTML = "状态：";
+                                tips.innerHTML = "正在将文件缓存...";
+                                reader.readAsArrayBuffer(filec.files[0]);
+                                reader.onload = function () {
+                                    var calcmethod = document.querySelector('#method').value;
+                                    tips.innerHTML = "获取到计算" + calcmethod + "值";
+                                    tips.innerHTML = "正在计算...";
+                                    var wordArray = CryptoJS.lib.WordArray.create(reader.result);
+                                    if (calcmethod == 'md5') {
+                                        var calchash = CryptoJS.MD5(wordArray).toString(CryptoJS.enc.Hex);
+                                    }
+                                    else {
+                                        var calchash = CryptoJS.SHA256(wordArray).toString(CryptoJS.enc.Hex);
+                                    }
+                                    console.log(calchash);
+                                    var f = document.getElementById('a').value;
+                                    var a = f.toLowerCase();
+                                    var b = calchash.toLowerCase();
+                                    var calcmethod = document.querySelector('#method').value;
+                                    if (b === a) {
+                                        tips.innerHTML = "校验成功<br>" + calcmethod + "值：" + calchash;
+                                        mdui.dialog({
+                                            title: '校验成功',
+                                            content: '请放心使用',
+                                            buttons: [
+                                                {
+                                                    text: '完成'
+                                                }
+                                            ]
+                                        });
+                                    }
+                                    else {
+                                        tips.innerHTML = "校验完成<br>" + calcmethod + "值：" + calchash;
+                                        mdui.dialog({
+                                            title: '校验失败',
+                                            content: '请检查获取的校验值是否有误（如多余的空格），或是校验值和校验方法不一致（如校验值是MD5，但校验方法是SHA256)；如果没有上述任一情况请自行解决',
+                                            buttons: [
+                                                {
+                                                    text: '确认'
+                                                }
+                                            ]
+                                        });
+                                    }
+                                };
+                            }
                         }
                     }
                 }
