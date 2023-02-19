c0c6cdb360228a8a287220e02d036b93f60a6799
Super12138 <70494801+Super12138@users.noreply.github.com>
2023-02-19 18:32:10 +0800

    添加自动更新功能

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { app, BrowserWindow, Menu, shell, ipcMain, dialog } = require('electron')
+const { autoUpdater } = require('electron-updater');
 const date = new Date()
 const year = date.getFullYear()
 
@@ -10,9 +11,7 @@ if (process.defaultApp) {
 } else {
   app.setAsDefaultProtocolClient('hash-checker')
 }
-
 const gotTheLock = app.requestSingleInstanceLock()
-
 if (!gotTheLock) {
   app.quit()
 } else {
@@ -34,6 +33,7 @@ function createWindow() {
     height: 800,
     center: true,
     // frame: false,
+    // titleBarStyle: 'hidden',
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false
@@ -42,116 +42,134 @@ function createWindow() {
   win.loadFile('index.html')
   // win.webContents.openDevTools();
   // win.setWindowButtonVisibility(true)
-  const wintemplate = [
-    {
-      label: '文件',
-      submenu: [
-        {
-          label: '关于',
-          role: 'about'
-        },
-        {
-          label: '退出',
-          role: 'quit'
+}
+const wintemplate = [
+  {
+    label: '文件',
+    submenu: [
+      {
+        label: '关于',
+        role: 'about'
+      },
+      {
+        label: '退出',
+        role: 'quit'
+      }
+    ]
+  },
+  {
+    label: '编辑',
+    submenu: [
+      {
+        label: '撤销',
+        role: 'undo'
+      },
+      {
+        label: '恢复',
+        role: 'redo'
+      },
+      {
+        label: '剪切',
+        role: 'cut'
+      },
+      {
+        label: '复制',
+        role: 'copy'
+      },
+      {
+        label: '粘贴',
+        role: 'paste'
+      },
+    ]
+  },
+  {
+    label: '开发者选项',
+    submenu: [
+      {
+        label: '打开开发者调试工具',
+        role: 'toggleDevTools'
+      },
+    ]
+  },
+  {
+    label: '帮助',
+    submenu: [
+      {
+        label: '使用说明',
+        click: async () => {
+          await shell.openExternal('https://github.com/Super12138/Hash-Checker')
         }
-      ]
-    },
-    {
-      label: '编辑',
-      submenu: [
-        {
-          label: '撤销',
-          role: 'undo'
-        },
-        {
-          label: '恢复',
-          role: 'redo'
-        },
-        {
-          label: '剪切',
-          role: 'cut'
-        },
-        {
-          label: '复制',
-          role: 'copy'
-        },
-        {
-          label: '粘贴',
-          role: 'paste'
-        },
-      ]
-    },
-    {
-      label: '帮助',
-      submenu: [
-        {
-          label: '使用说明',
-          click: async () => {
-            await shell.openExternal('https://github.com/Super12138/Hash-Checker')
-          }
-        }
-      ]
-    }
-  ]
-  const darwintemplate = [
-    {
-      label: app.name,
-      submenu: [
-        {
-          label: '关于',
-          role: 'about'
-        },
-        {
-          label: '退出',
-          role: 'quit'
-        }
-      ]
-    },
-    {
-      label: '编辑',
-      submenu: [
-        {
-          label: '撤销',
-          role: 'undo'
-        },
-        {
-          label: '恢复',
-          role: 'redo'
-        },
-        {
-          label: '剪切',
-          role: 'cut'
-        },
-        {
-          label: '复制',
-          role: 'copy'
-        },
-        {
-          label: '粘贴',
-          role: 'paste'
-        },
-      ]
-    },
-    {
-      label: '帮助',
-      submenu: [
-        {
-          label: '使用说明',
-          click: async () => {
-            await shell.openExternal('https://github.com/Super12138/Hash-Checker')
-          }
-        }
-      ]
-    }
-  ]
-  if (process.platform == 'darwin') {
-    const dmenu = Menu.buildFromTemplate(darwintemplate)
-    Menu.setApplicationMenu(dmenu)
+      }
+    ]
   }
-  else {
-    const wmenu = Menu.buildFromTemplate(wintemplate)
-    Menu.setApplicationMenu(wmenu)
+]
+const darwintemplate = [
+  {
+    label: app.name,
+    submenu: [
+      {
+        label: '关于',
+        role: 'about'
+      },
+      {
+        label: '退出',
+        role: 'quit'
+      }
+    ]
+  },
+  {
+    label: '编辑',
+    submenu: [
+      {
+        label: '撤销',
+        role: 'undo'
+      },
+      {
+        label: '恢复',
+        role: 'redo'
+      },
+      {
+        label: '剪切',
+        role: 'cut'
+      },
+      {
+        label: '复制',
+        role: 'copy'
+      },
+      {
+        label: '粘贴',
+        role: 'paste'
+      },
+    ]
+  },
+  {
+    label: '开发者选项',
+    submenu: [
+      {
+        label: '打开开发者调试工具',
+        role: 'toggleDevTools'
+      },
+    ]
+  },
+  {
+    label: '帮助',
+    submenu: [
+      {
+        label: '使用说明',
+        click: async () => {
+          await shell.openExternal('https://github.com/Super12138/Hash-Checker')
+        }
+      }
+    ]
   }
+]
+if (process.platform == 'darwin') {
+  const dmenu = Menu.buildFromTemplate(darwintemplate)
+  Menu.setApplicationMenu(dmenu)
+}
+else {
+  const wmenu = Menu.buildFromTemplate(wintemplate)
+  Menu.setApplicationMenu(wmenu)
 }
 
 app.whenReady().then(() => {
@@ -164,11 +182,50 @@ app.whenReady().then(() => {
   })
 })
 
+// 自动更新
+// 在应用程序准备好后，检查更新并通知用户
+app.on('ready', function () {
+  autoUpdater.checkForUpdatesAndNotify();
+});
+
+// 接收来自更新程序的错误消息
+autoUpdater.on('error', function (error) {
+  dialog.showErrorBox('错误: ', error == null ? "unknown" : (error.stack || error).toString());
+});
+
+// 接收来自更新程序的更新可用消息
+autoUpdater.on('update-available', function () {
+  dialog.showMessageBox({
+    type: 'info',
+    title: '检测到新版本',
+    message: '检测到有新版本，是否下载更新（更新详情见 GitHub Release ）？',
+    buttons: ['是', '否']
+  }, function (buttonIndex) {
+    if (buttonIndex === 0) {
+      autoUpdater.downloadUpdate();
+    }
+  });
+});
+
+// 更新程序已下载并准备好安装时，通知用户并退出并安装应用程序
+autoUpdater.on('update-downloaded', function () {
+  dialog.showMessageBox({
+    type: 'info',
+    title: '更新下载完成',
+    message: '更新已下载完成，是否退出应用并更新？',
+    buttons: ['是', '否']
+  }, function (buttonIndex) {
+    if (buttonIndex === 0) {
+      autoUpdater.quitAndInstall();
+    }
+  });
+});
+
 app.setAboutPanelOptions({
   applicationName: 'Hash Checker',
-  applicationVersion: '1.0.4',
+  applicationVersion: '1.0.5-beta',
   copyright: 'Copyright © 2019-' + year + ' Super12138',
-  version: '1040'
+  version: '1050'
 })
 
 app.on('window-all-closed', () => {
