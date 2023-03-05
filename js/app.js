84cb596dc83ef72608d4890632ca4d9d86837582
Super12138 <70494801+Super12138@users.noreply.github.com>
2023-03-05 11:21:07 +0800

    优化应用执行逻辑

--- js/app.js
+++ js/app.js
@@ -71,107 +71,82 @@ function getfilename() {
 }
 
 function calch() {
-    var file = document.querySelector('#getfile').files[0];
+    const file = document.querySelector('#getfile').files[0];
     if (!file || file.length == 0) {
-        ipcRenderer.send('no-file')
+        ipcRenderer.send('no-file');
+        return;
     }
-    else {
-        var meth = document.querySelector('#method').value;
-        var mod = document.querySelector('#mod').value;
-        if (meth == "nullselect" && mod == "nullselect") {
-            ipcRenderer.send('no-modmeth')
+    const meth = document.querySelector('#method').value;
+    const mod = document.querySelector('#mod').value;
+    if (meth == "nullselect" && mod == "nullselect") {
+        ipcRenderer.send('no-modmeth');
+        return;
+    }
+    const only = document.querySelector('#mod').value;
+    console.log(only);
+    if (only == "nullselect") {
+        ipcRenderer.send('no-model');
+        return;
+    }
+    if (meth == "nullselect") {
+        ipcRenderer.send('no-method');
+        return;
+    }
+    if (only == "generate") {
+        if (meth == "nullselect") {
+            ipcRenderer.send('no-method');
+            return;
         }
-        else {
-            var only = document.querySelector('#mod').value;
-            console.log(only);
-            if (only == "nullselect") {
-                ipcRenderer.send('no-model')
+        const filec = document.querySelector('#getfile');
+        const ttitle = document.querySelector('#ttitle');
+        const tips = document.querySelector('#tips');
+        const isClipboard = document.querySelector('#isclipboard').checked;
+        const reader = new FileReader();
+        ttitle.innerHTML = "状态：";
+        tips.innerHTML = "正在将文件缓存...";
+        reader.readAsArrayBuffer(filec.files[0]);
+        reader.onload = ()=>{
+            const calcmethod = document.querySelector('#method').value;
+            tips.innerHTML = "获取到计算" + calcmethod + "值";
+            tips.innerHTML = "正在计算...";
+            const wordArray = CryptoJS.lib.WordArray.create(reader.result);
+            const calchash = calcmethod == 'md5' ? CryptoJS.MD5(wordArray).toString(CryptoJS.enc.Hex) : CryptoJS.SHA256(wordArray).toString(CryptoJS.enc.Hex);
+            console.log(calchash);
+            if (isClipboard) {
+                clipboard.writeText(calchash);
+                tips.innerHTML = calcmethod + "计算完成<br>" + calcmethod + "值：" + "<code>" + calchash + "</code>";
             }
-            else {
-                var meth = document.querySelector('#method').value;
-                if (meth == "nullselect") {
-                    ipcRenderer.send('no-method')
-                } else {
-                    if (only == "generate") {
-                        var meth = document.querySelector('#method').value;
-                        if (meth == "nullselect") {
-                            ipcRenderer.send('no-method')
-                        }
-                        else {
-                            var filec = document.querySelector('#getfile');
-                            var ttitle = document.querySelector('#ttitle');
-                            var tips = document.querySelector('#tips');
-                            let reader = new FileReader();
-                            ttitle.innerHTML = "状态：";
-                            tips.innerHTML = "正在将文件缓存...";
-                            reader.readAsArrayBuffer(filec.files[0]);
-                            reader.onload = function () {
-                                var calcmethod = document.querySelector('#method').value;
-                                tips.innerHTML = "获取到计算" + calcmethod + "值";
-                                tips.innerHTML = "正在计算...";
-                                var wordArray = CryptoJS.lib.WordArray.create(reader.result);
-                                if (calcmethod == 'md5') {
-                                    var calchash = CryptoJS.MD5(wordArray).toString(CryptoJS.enc.Hex);
-                                }
-                                else {
-                                    var calchash = CryptoJS.SHA256(wordArray).toString(CryptoJS.enc.Hex);
-                                }
-                                console.log(calchash);
-                                var calcmethod = document.querySelector('#method').value;
-                                clipboard.writeText(calchash);
-                                tips.innerHTML = "计算完成，" + calcmethod + "值已写入您的剪贴板！<br>" + calcmethod + "值：" + "<code>" + calchash + "</code>";
-                                ipcRenderer.send('gen-ok')
-                            }
-                        }
-                    }
-                    else {
-                        var meth = document.querySelector('#method').value;
-                        if (meth == "nullselect") {
-                            ipcRenderer.send('no-meth')
-                        }
-                        else {
-                            var inpc = document.getElementById('a').value;
-                            if (inpc === "" || inpc === null || inpc === undefined) {
-                                ipcRenderer.send('no-checksum')
-                            }
-                            else {
-                                var filec = document.querySelector('#getfile');
-                                var ttitle = document.querySelector('#ttitle');
-                                var tips = document.querySelector('#tips');
-                                let reader = new FileReader();
-                                ttitle.innerHTML = "状态：";
-                                tips.innerHTML = "正在将文件缓存...";
-                                reader.readAsArrayBuffer(filec.files[0]);
-                                reader.onload = function () {
-                                    var calcmethod = document.querySelector('#method').value;
-                                    tips.innerHTML = "获取到计算" + calcmethod + "值";
-                                    tips.innerHTML = "正在计算...";
-                                    var wordArray = CryptoJS.lib.WordArray.create(reader.result);
-                                    //化简代码
-                                    if (calcmethod == 'md5') {
-                                        var calchash = CryptoJS.MD5(wordArray).toString(CryptoJS.enc.Hex);
-                                    }
-                                    else {
-                                        var calchash = CryptoJS.SHA256(wordArray).toString(CryptoJS.enc.Hex);
-                                    }
-                                    console.log(calchash);
-                                    var f = document.getElementById('a').value;
-                                    var a = f.toLowerCase();
-                                    var b = calchash.toLowerCase();
-                                    var calcmethod = document.querySelector('#method').value;
-                                    if (b === a) {
-                                        tips.innerHTML = "校验成功<br>文件" + calcmethod + "值：" + "<code>" + calchash + "</code>";
-                                        ipcRenderer.send('check-ok')
-                                    }
-                                    else {
-                                        tips.innerHTML = "校验失败<br>文件" + calcmethod + "值：" + "<code>" + calchash + "</code>" + "<br>请检查获取的校验值是否有误（如多余的空格，复制时少复制了字符），或是校验值和校验方法不一致（如校验值是MD5，但校验方法是SHA256)；如果没有上述任一情况请自行解决";
-                                        ipcRenderer.send('check-fail')
-                                    }
-                                };
-                            }
-                        }
-                    }
-                }
+            tips.innerHTML = "计算完成，" + calcmethod + "值已写入您的剪贴板！<br>" + calcmethod + "值：" + "<code>" + calchash + "</code>";
+            ipcRenderer.send('gen-ok');
+        }
+    } else {
+        const inpc = document.getElementById('a').value;
+        if (!inpc) {
+            ipcRenderer.send('no-checksum');
+            return;
+        }
+        const filec = document.querySelector('#getfile');
+        const ttitle = document.querySelector('#ttitle');
+        const tips = document.querySelector('#tips');
+        const reader = new FileReader();
+        ttitle.innerHTML = "状态：";
+        tips.innerHTML = "正在将文件缓存...";
+        reader.readAsArrayBuffer(filec.files[0]);
+        reader.onload = ()=>{
+            const calcmethod = document.querySelector('#method').value;
+            tips.innerHTML = "获取到计算" + calcmethod + "值";
+            tips.innerHTML = "正在计算...";
+            const wordArray = CryptoJS.lib.WordArray.create(reader.result);
+            const calchash = calcmethod == 'md5' ? CryptoJS.MD5(wordArray).toString(CryptoJS.enc.Hex) : CryptoJS.SHA256(wordArray).toString(CryptoJS.enc.Hex);
+            console.log(calchash);
+            const f = document.getElementById('a').value.toLowerCase();
+            const b = calchash.toLowerCase();
+            if (b === f) {
+                tips.innerHTML = "校验成功<br>文件" + calcmethod + "值：" + "<code>" + calchash + "</code>";
+                ipcRenderer.send('check-ok');
+            } else {
+                tips.innerHTML = "校验失败<br>文件" + calcmethod + "值：" + "<code>" + calchash + "</code>" + "<br>请检查获取的校验值是否有误（如多余的空格，复制时少复制了字符），或是校验值和校验方法不一致（如校验值是MD5，但校验方法是SHA256)；如果没有上述任一情况请自行解决";
+                ipcRenderer.send('check-fail');
             }
         }
     }
