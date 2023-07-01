a97a09180c986893d36ebed1872c990f548de371
Super12138 <70494801+Super12138@users.noreply.github.com>
2023-07-01 10:39:46 +0800

    修复移动端无法发送通知的问题

--- src/utils/notification.js
+++ src/utils/notification.js
@@ -2,6 +2,23 @@ export function sendNotification(title, content) {
     const options = {
         body: content,
         lang: "zh-Hans-CN",
+    };
+
+    if ('Notification' in window && 'serviceWorker' in navigator) {
+        if (Notification.permission === 'granted') {
+            navigator.serviceWorker.ready.then((registration) => {
+                registration.showNotification(title, options);
+            });
+        } else if (Notification.permission !== 'denied') {
+            Notification.requestPermission().then((permission) => {
+                if (permission === 'granted') {
+                    navigator.serviceWorker.ready.then(function (registration) {
+                        registration.showNotification(title, options);
+                    });
+                }
+            });
+        }
+    } else {
+        console.log('浏览器不支持通知');
     }
-    const notification = new Notification(title, options);
 }
