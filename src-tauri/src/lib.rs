32368d323f345427ff28db7c38250d939ba37d80
Super12138 <70494801+Super12138@users.noreply.github.com>
2025-08-26 09:23:57 +0800

    fix: 删除无用多余的代码

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -4,8 +4,6 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_notification::init())
-        .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
